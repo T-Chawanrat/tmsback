d8536cc6a1096665a3b8489a427a478271639686
T-Chawanrat <thip.chawanrat@gmail.com>
2025-11-22 19:43:02 +0700

    add autofill address

--- controllers/filterController.js
+++ controllers/filterController.js
@@ -18,3 +18,31 @@ export const getCustomers = async (req, res) => {
     res.status(500).json({ message: "An error occurred" });
   }
 };
+
+export const getWarehouses = async (req, res) => {
+  try {
+    const { zip_code } = req.query; // 👈 รับ zip_code จาก query
+
+    let sql = `
+      SELECT *
+      FROM admin_tmg.master_warehouses
+    `;
+    const params = [];
+
+    if (zip_code) {
+      sql += ` WHERE zip_code = ?`;
+      params.push(zip_code);
+    }
+
+    const [rows] = await db.query(sql, params);
+
+    res.json({
+      data: rows,
+      count: rows.length,
+    });
+  } catch (err) {
+    console.error("getWarehouses error:", err);
+    res.status(500).json({ message: "An error occurred" });
+  }
+};
+
--- routes/filterRoute.js
+++ routes/filterRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getCustomers } from "../controllers/filterController.js";
+import { getCustomers, getWarehouses } from "../controllers/filterController.js";
 
 const router = express.Router();
 
 router.get("/customers", getCustomers);
+router.get("/warehouses", getWarehouses);
 
 export default router;
